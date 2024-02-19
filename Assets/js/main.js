d05bb702f7df168d3ab97dc9968cf84f2c3fddb7
UmamageshwaranK <umamageshwarank6@gmail.com>
2024-02-19 18:42:43 +0530

    store the data into localStorage

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,12 +1,14 @@
-// js validation using plugins
 import JustValidate from "just-validate";
 
+// js validation using plugins
 const formEl1 = document.getElementById("formEl");
-console.log(formEl1);
-// using instance of JustValidate
-const formValidation = new JustValidate(formEl1);
+// console.log(formEl1);
+// using instance of JustValidate && check before validation
+const formValidation = new JustValidate(formEl1, {
+  validateBeforeSubmitting: true,
+});
 
-console.log(formValidation);
+// console.log(formValidation);
 // lets use Plugins
 formValidation.addField(
   "#name",
@@ -30,15 +32,16 @@ formValidation.addField(
 formValidation.addField(
   "#phone",
   [
+    { rule: "required" },
     {
       rule: "number",
     },
     {
-      rule: "minNumber",
+      rule: "minLength",
       value: 10,
     },
     {
-      rule: "maxNumber",
+      rule: "maxLength",
       value: 10,
     },
   ],
@@ -46,3 +49,39 @@ formValidation.addField(
     errorLabelCssClass: ["form-error"],
   }
 );
+
+formValidation.onSuccess(() => {
+  console.log("submited");
+  // FormData  get value from this one
+  const formData = new FormData(formEl1);
+  // console.log(formEl1);
+  // console.log(formData);
+  // FormData means  whatever forms inside body tag we can access the form data .
+  // first create instance of the form data and  then use the inbuild formdata method is called entries().
+  // In this method it will return key and values pair contain inside a form data .
+
+  // Object.fromEnteries  its convert the array data into object format
+  const formvalueObj = Object.fromEntries(formData.entries());
+
+  // use json to store value  in local storage
+  // const dataJson = JSON.stringify(formvalueObj);
+  // console.log(dataJson);
+  // just converted into json value
+  // json stands for javasript object notation its used to store the data key  and value pair. and then its change datatype to string type
+  // whenever use the json to use the method of  stringfy()
+  // And whenever the data reterive from the database use the  parse() method
+
+  // lets create Array into an object using  fresh object data
+  // lets push into the array format
+
+  // exiting Data and check the data empty create an array otherwise not
+  //  lets check the value havent in the localstorage
+  let existingData = localStorage.getItem("courierData")
+    ? JSON.parse(localStorage.getItem("courierData"))
+    : [];
+  // console.log(existingData);
+  existingData.push(formvalueObj);
+  // lets test our code
+  console.log(existingData);
+  localStorage.setItem("courierData", JSON.stringify(existingData));
+});
